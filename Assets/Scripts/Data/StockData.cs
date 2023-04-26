cdf9ed79abe3f39bd71da4d5ce6099cf962fd09c
Mitsuki <kukurumi9963@softbank.ne.jp>
2023-04-26 14:20:02 +0900

    初期

---
+++ Assets/Scripts/Data/StockData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGCreateNow_Local.Data
+{
+    public class StockData : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+}
